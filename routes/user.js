b84f0b3fec6ff0acd071de81e0c8c4f4e7356371
quabid <quabidlord@gmail.com>
2021-05-27 21:03:57 -0400

    Added route protection

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import * as UC from "../controllers/userController.js";
+import { protect } from "../middleware/AuthMiddleware.js";
 
 const userRouter = Router();
 
-userRouter.route("/pwd").post(UC.updateUserPassword);
-userRouter.route("/email").post(UC.updateUserEmail);
-userRouter.route("/profile/update").post(UC.updateUserProfile);
-userRouter.route("/profile/create").post(UC.createUserProfile);
+userRouter.route("/pwd").post(protect, UC.updateUserPassword);
+userRouter.route("/email").post(protect, UC.updateUserEmail);
+userRouter.route("/profile/update").post(protect, UC.updateUserProfile);
+userRouter.route("/profile/create").post(protect, UC.createUserProfile);
 
 export default userRouter;
