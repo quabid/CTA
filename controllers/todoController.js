6bd71c36d69f218297da11e8b24c262d8ebcd505
quabid <quabidlord@gmail.com>
2021-06-07 17:44:21 -0400

    Modified todo route & controller

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -34,16 +34,94 @@ export const addTodo = asyncHandler(async (req, res) => {
   });
 });
 
-// @desc        Get a user's todos
-// @route       GET /api/todos
+// @desc        Delete a user's todo
+// @route       POST /api/todos/remove
 // @access      Private
-export const getTodos = asyncHandler(async (req, res) => {
+export const removeTodo = asyncHandler(async (req, res) => {
   logger.info(
-    `Export: getTodos, Route: /api/todos, Method: GET, Requested URL: ${req.url}`
+    `Export: removeTodo, Route: /api/todos/remove, Method: POST, Requested URL: ${req.url}`
   );
 
   res.status(200).json({
     status: "success",
     url: req.url,
+  });
+});
+
+// @desc        Get a user's single todo
+// @route       POST /api/todos/id
+// @access      Private
+export const getTodo = asyncHandler(async (req, res) => {
+  logger.info(
+    `Export: getTodo, Route: /api/todos/id, Method: GET, Requested URL: ${req.url}`
+  );
+
+  const tid = req.params.tid;
+
+  res.status(200).json({
+    status: "success",
+    url: req.url,
+    todoId: tid,
+  });
+});
+
+// @desc        Get a author's todos
+// @route       GET /api/todos/author
+// @access      Private
+export const getTodos = asyncHandler(async (req, res) => {
+  logger.info(
+    `Export: getTodos, Route: /api/todos/author, Method: GET, Requested URL: ${req.url}`
+  );
+
+  const author = req.params.author;
+
+  res.status(200).json({
+    status: "success",
+    url: req.url,
+    author: author,
+  });
+});
+
+// @desc        Update a user's todo
+// @route       POST /api/todos/update
+// @access      Private
+export const updateTodo = asyncHandler(async (req, res) => {
+  logger.info(
+    `Export: updateTodo, Route: /api/todos/update, Method: POST, Requested URL: ${req.url}`
+  );
+
+  const oldTodo = {};
+  if (req.body.todoId) {
+    oldTodo.todoId = req.body.todoId;
+  }
+
+  if (req.body.todoRev) {
+    oldTodo.todoRev = req.body.todoRev;
+  }
+
+  if (req.body.todoAuthor) {
+    oldTodo.todoAuthor = req.body.todoAuthor;
+  }
+
+  if (req.body.todoStart) {
+    oldTodo.start = req.body.todoStart;
+  }
+
+  if (req.body.todoEnd) {
+    oldTodo.end = req.body.todoEnd;
+  }
+
+  if (req.body.title) {
+    oldTodo.title = req.body.title;
+  }
+
+  if (req.body.body) {
+    oldTodo.body = req.body.body;
+  }
+
+  res.status(200).json({
+    status: "success",
+    url: req.url,
+    payload: oldTodo,
   });
 });
--- routes/todo.js
+++ routes/todo.js
@@ -6,6 +6,12 @@ const todoRouter = Router();
 
 todoRouter.route("/add").post(protect, TC.addTodo);
 
-todoRouter.route("/").get(protect, TC.getTodos);
+todoRouter.route("/remove").post(protect, TC.removeTodo);
+
+todoRouter.route("/update").post(protect, TC.updateTodo);
+
+todoRouter.route("/:author").get(protect, TC.getTodos);
+
+todoRouter.route("/:tid").post(protect, TC.getTodo);
 
 export default todoRouter;
